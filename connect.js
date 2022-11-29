8496c708d225ea78394988162cf8da45538bcd9e
SeanDylan1982 <74496368+SeanDylan1982@users.noreply.github.com>
2022-11-29 13:22:49 +0200

    mongodb auth providers added

--- connect.js
+++ connect.js
@@ -1,4 +1,4 @@
-import { Mongodb } from mongodb;
+import { mongodb } from mongodb;
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://mongo:LL0921jj@cluster0.n9baelp.mongodb.net/?retryWrites=true&w=majority";
@@ -6,5 +6,6 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const collection = client.db("users").collection("bbdb");
   // perform actions on the collection object
+  
   client.close();
 });
