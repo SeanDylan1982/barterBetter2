ebef5e592d7e59883e752902eea53c8a2dfd2f52
SeanDylan1982 <74496368+SeanDylan1982@users.noreply.github.com>
2022-12-03 13:50:17 +0200

    register users

---
+++ users.js
@@ -0,0 +1,15 @@
+const usersObj = [
+  {
+    id: 1,
+    name: "sean",
+    surname: "patterson",
+    username: "seandylan1982",
+    email: "seandylanaptterson@gmail.com",
+    password: "1234",
+    confirm: "1234",
+    region: ["eastern cape", "free state","gauteng", "kwa-zulu natal", "limpopo", "mpumulanga", "northern cape", "north west", "western cape"],
+    category: ["collections", "clothing","tools", "fresh produce", "electronics", "services", "miscellaneous"],
+    image: "./img/profile-1.png",
+    timestamp: ""
+  },
+];
